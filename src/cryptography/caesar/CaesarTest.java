b34dec566a80b78ac6b4f7b1de41f4278286b372
Norkator <norkator@gmail.com>
2020-04-22 22:08:30 +0300

    Fix tests, add scytale cipher

--- src/cryptography/caesar/CaesarTest.java
+++ src/cryptography/caesar/CaesarTest.java
@@ -1,15 +1,16 @@
+/**
+ * This class is part of running automated tests
+ */
 package cryptography.caesar;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import cryptography.Mode;
 
-public class CaesarJUnit {
+public class CaesarTest {
 
 	@Test
-	@DisplayName("Testing Caesar encrypt and decrypt methods")
-	void runTest() {
+	public void runTest() {
 		assertEquals("KHOOR", Caesar.caesar("HELLO", 3, Mode.ENCRYPT));
 		assertEquals("HELLO", Caesar.caesar("KHOOR", 3, Mode.DECRYPT));	
 	}
