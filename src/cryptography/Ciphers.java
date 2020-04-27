3664e35369a9867f51ad8fe3d7dbe4de3e6af9ed
Norkator <norkator@gmail.com>
2020-04-27 19:41:03 +0300

    More ciphers
    Added more ciphers till realized that this needs more structure like ciphers, hashes, encodings.

--- src/cryptography/Ciphers.java
+++ src/cryptography/Ciphers.java
@@ -14,6 +14,9 @@ import cryptography.vigenere.Vigenere;
 import cryptography.bacon.Bacon;
 import cryptography.chaocipher.Chaocipher;
 import cryptography.gronsfeld.Gronsfeld;
+import cryptography.adfgvx.Adfgvx;
+import cryptography.a1z26.A1z26;
+import cryptography.playfair.Playfair;
 
 public class Ciphers {
 
@@ -64,6 +67,18 @@ public class Ciphers {
 		// Gronsfeld
 		System.out.println("Gronsfeld encrypt: " + Gronsfeld.gronsfeld("HELLO", "15243", Mode.ENCRYPT));
 		System.out.println("Gronsfeld decrypt: " + Gronsfeld.gronsfeld("IJNPR", "15243", Mode.DECRYPT));
+
+		// Adfgvx
+		System.out.println("Adfgvx encrypt: " + Adfgvx.adfgvx("HELLOWORLD", Mode.ENCRYPT));
+		System.out.println("Adfgvx decrypt: " + Adfgvx.adfgvx("DDAVDXDXFFGVFFFXDXAG", Mode.DECRYPT));
+
+		// A1z26
+		System.out.println("A1z26 encrypt: " + A1z26.a1z26("HELLO", Mode.ENCRYPT));
+		System.out.println("A1z26 decrypt: " + A1z26.a1z26("8-5-12-12-15", Mode.DECRYPT));
+		
+		// Playfair
+		System.out.println("Playfair encrypt: " + Playfair.playfair("HELLOWORLDTEST", "ISFAIR", true, Mode.ENCRYPT));
+		System.out.println("Playfair decrypt: " + Playfair.playfair("MBQFHQVPFNEQCAQY", "ISFAIR", true, Mode.DECRYPT));
 
 	}
 
