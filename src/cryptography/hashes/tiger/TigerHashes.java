51df8cfcd09aacbbab5e498ca4d10e1fc780bfd5
Norkator <norkator@gmail.com>
2020-04-29 16:51:59 +0300

    Adding bases of more hashing methods

---
+++ src/cryptography/hashes/tiger/TigerHashes.java
@@ -0,0 +1,40 @@
+package cryptography.hashes.tiger;
+
+import java.security.NoSuchAlgorithmException;
+import jonelo.jacksum.JacksumAPI;
+import jonelo.jacksum.algorithm.AbstractChecksum;
+
+public class TigerHashes {
+
+	public static void main(String[] args) {
+	}
+
+	private static String jacksumAPI(String input, String instance) {
+		String output = "";
+		AbstractChecksum abstractChecksum = null;
+		try {
+			abstractChecksum = JacksumAPI.getChecksumInstance(instance.toLowerCase());
+			abstractChecksum.update(input.getBytes());
+			output = abstractChecksum.getFormattedValue();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return output;
+	}
+
+	public static String tiger(String input) {
+		return jacksumAPI(input, "Tiger");
+	}
+
+	public static String tiger2(String input) {
+		return jacksumAPI(input, "Tiger2");
+	}
+	
+	public static String tiger128(String input) {
+		return jacksumAPI(input, "Tiger-128");
+	}
+
+	public static String tiger160(String input) {
+		return jacksumAPI(input, "Tiger-160");
+	}
+}
