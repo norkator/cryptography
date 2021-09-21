dde5a49380361ba8ee513c4a033d9f4e7cd96370
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-09-21 22:59:23 +0300

    Folder "package" path name must be lower case

---
+++ src/cryptography/ciphers/vic/VICTest.java
@@ -0,0 +1,34 @@
+package cryptography.ciphers.vic;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class VICTest {
+
+	@Test
+	public void runTest() {
+
+		int date = 741776;
+		int keyGroup = 77651;
+		int personalNo = 8;
+		
+		assertEquals("1205348679",VIC.keyGen("I dream of Jeannie with t", date, personalNo, keyGroup));
+		
+		String plainText = "We are pleased to hear of your success in establishing your false identity You will be sent some money to cover expenses within a month";
+		
+		String cipherText = VIC.encrypt(plainText, 
+						"AT ONE SIR", 
+						"I dream of Jeannie with t", 
+						date, personalNo, keyGroup);
+		
+
+		String decipherText = VIC.decrypt(cipherText, 
+						"AT ONE SIR", 
+						"I dream of Jeannie with t", 
+						date, personalNo, keyGroup);
+
+		assertEquals(plainText.toUpperCase().replaceAll(" ", ""), decipherText);
+	}
+}
