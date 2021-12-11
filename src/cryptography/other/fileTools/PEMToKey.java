e4a709c88cd33cefbae47e97f27e0020e2079312
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-12-11 14:51:59 +0200

    use mime decoder

--- src/cryptography/other/fileTools/PEMToKey.java
+++ src/cryptography/other/fileTools/PEMToKey.java
@@ -1,13 +1,15 @@
 package cryptography.other.fileTools;
 
 import org.spongycastle.jce.provider.BouncyCastleProvider;
-import sun.misc.BASE64Decoder;
 
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 
 public class PEMToKey {
+
+	private static Base64.Decoder decoder = Base64.getMimeDecoder();
 
 	public static PrivateKey getPemPrivateKey(String pemContents, String algorithm) throws Exception {
 		String privateKeyPEM = pemContents.replace("-----BEGIN PRIVATE KEY-----", "");
@@ -15,8 +17,7 @@ public class PEMToKey {
 		privateKeyPEM = privateKeyPEM.replace("-----BEGIN EC PRIVATE KEY-----", "");
 		privateKeyPEM = privateKeyPEM.replace("-----END EC PRIVATE KEY-----", "");
 
-		BASE64Decoder b64 = new BASE64Decoder();
-		byte[] decoded = b64.decodeBuffer(privateKeyPEM);
+		byte[] decoded = decoder.decode(privateKeyPEM);
 
 		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
 		KeyFactory kf = KeyFactory.getInstance(algorithm, new BouncyCastleProvider());
