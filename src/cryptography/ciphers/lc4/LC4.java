d89e35f7dfbb34539c3f948850215fa53da2e07a
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2022-06-09 22:31:10 +0300

    adding skeletons

--- src/cryptography/ciphers/lc4/LC4.java
+++ src/cryptography/ciphers/lc4/LC4.java
@@ -1,8 +1,35 @@
 package cryptography.ciphers.lc4;
 
+/**
+ * Based on https://github.com/umcconnell/lc4/blob/master/src/lc4.js
+ */
 public class LC4 {
 
+	public static String encrypt(String msg) {
 
-	
+		return null;
+	}
+
+
+	public static String decrypt(String msg) {
+
+		return null;
+	}
+
+
+	private static void shuffle() {
+	}
+
+	private static void randomElement() {
+	}
+
+	private static void shiftRowRight() {
+	}
+
+	private static void shiftColumnDown() {
+	}
+
+	private static void position() {
+	}
 
 }
---
+++ src/cryptography/ciphers/lc4/LC4Test.java
@@ -0,0 +1,13 @@
+package cryptography.ciphers.lc4;
+
+import org.junit.Test;
+
+public class LC4Test {
+
+	@Test
+	public void runTest() {
+		// assertEquals("...", LC4.encrypt("testing"));
+		// assertEquals("testing", LC4.decrypt("..."));
+	}
+
+}
